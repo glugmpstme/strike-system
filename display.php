d4f7ad0c9dd82783bd545478290f283a19bdc782
Pranjali Basmatkar <basmatkarpranjali@gmail.com>
2016-11-10 01:26:58 +0530

    Updated
    fixed typos

--- display.php
+++ display.php
@@ -1,10 +1,10 @@
 <?php
-mysql_connect(,,) or die("Connection Failed");
+mysql_connect("localhost","root") or die("Connection Failed");
 mysql_select_db("strike-system") or die("Connection Failed");
-$name = $_POST['name'];
-$username = $_POST['username'];
+$name = $_POST['txtname'];
+$username = $_POST['txtusername'];
 $query = "select strikes from database where name = '$name' and username = '$username'"
-$ans = mysql_query($query);
+$strikes = mysql_query($query);
 while ($line = mysql_fetch_array($ans, MYSQL_ASSOC)){
   echo $line['name'];
   echo $line['strikes'];
